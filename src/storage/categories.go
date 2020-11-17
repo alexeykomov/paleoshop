f4b880cc3b365745380a1e7605b8a0162b65232b
alexeykomov <alexeykofficial@gmail.com>
2020-11-18 01:00:10 +0300

    add: [WIP] methods to get categories

--- src/storage/categories.go
+++ src/storage/categories.go
@@ -2,11 +2,45 @@ package storage
 
 import "database/sql"
 
-func GetCategories() (sql.Result, error) {
+type Category struct {
+	id              int
+	title           string
+	coverURL        string
+	childCategories []Category
+	childProducts   []product
+}
+
+type product struct {
+	id          int
+	title       string
+	coverURL    string
+	description string
+}
+
+func GetRootCategory() (Category, error) {
 	db, err := Connection()
 	if err != nil {
-		return nil, err
+		return Category{}, err
 	}
-	rows, err := db.Exec("SELECT * FROM CATEGORIES WHERE ancestry = ''")
-	rows
+	row := db.QueryRow("SELECT id, title, cover FROM CATEGORIES " +
+		"WHERE ancestry = ''")
+	var rootCategory Category
+	err = row.Scan(&rootCategory.id, &rootCategory.title, &rootCategory.coverURL)
+	if err != nil {
+		return Category{}, err
+	}
+	return rootCategory, nil
+}
+
+func GetSubcategoriesOfCategory(categoryId int) (Category, error) {
+	db, err := Connection()
+	if err != nil {
+		return Category{}, err
+	}
+	rows, err := db.Query("SELECT * FROM CATEGORIES " +
+		"WHERE ancestry LIKE %id%")
+	var category Category
+	for rows.Next() {
+		err = rows.Scan(&category.id, &category.title, &category.coverURL)
+	}
 }
