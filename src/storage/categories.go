45c6b8c5813b6fc3bc91a81ec87529c7f0e8597d
alexeykomov <alexeykofficial@gmail.com>
2020-11-26 00:53:15 +0300

    add: get subcategories method

--- src/storage/categories.go
+++ src/storage/categories.go
@@ -2,6 +2,7 @@ package storage
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -10,14 +11,21 @@ type Category struct {
 	title           string
 	coverURL        string
 	childCategories []Category
-	childProducts   []product
+	childProducts   []Product
+	ancestorIds []int
 }
 
-type product struct {
+type Product struct {
 	id          int
 	title       string
 	coverURL    string
 	description string
+	pictures []Picture
+}
+
+type Picture struct {
+	id int
+	url string
 }
 
 func GetRootCategory() (Category, error) {
@@ -35,21 +43,47 @@ func GetRootCategory() (Category, error) {
 	return rootCategory, nil
 }
 
-func GetSubcategoriesOfCategory(ancestorAndCurrentIds []string) ([]Category, error) {
+func GetSubcategoriesOfCategory(categoryId string) ([]Category, error) {
 	db, err := Connection()
 	if err != nil {
 		return []Category{}, err
 	}
 	rows, err := db.Query(
-		fmt.Sprintf("select products.id, ancestry, products.title "+
-			"from categories, products, category_products cp where "+
-			"categories.id = cp.category_id and "+
-			"products.id = cp.product_id and ancestry like '/%s%%'", strings.Join(ancestorAndCurrentIds, "/")))
+		fmt.Sprintf("select id, ancestry, title, cover from categories" +
+			" where id %s", categoryId))
 	var category Category
 	var categories []Category
+	var ancestry string
 	for rows.Next() {
-		err = rows.Scan(&category.id, &category.title, &category.coverURL)
+		err = rows.Scan(&category.id, &ancestry, &category.title, &category.coverURL)
+		for _, id := range strings.Split(ancestry, "/") {
+			idInt, err := strconv.Atoi(id)
+			if err != nil {
+				continue
+			}
+			category.ancestorIds = append(category.ancestorIds, idInt)
+		}
 		categories = append(categories, category)
 	}
 	return categories, nil
+}
+
+func GetProductsOfCategory(ancestorIds []string, categoryId string) ([]Product, error) {
+	db, err := Connection()
+	if err != nil {
+		return []Product{}, err
+	}
+	rows, err := db.Query(
+		fmt.Sprintf("select products.id, products.title, products.cover "+
+			"from categories, products, category_products cp where "+
+			"categories.id = cp.category_id and "+
+			"products.id = cp.product_id and ancestry like '/%s%%'",
+			strings.Join(append(ancestorIds, categoryId), "/")))
+	var product Product
+	var products []Product
+	for rows.Next() {
+		err = rows.Scan(&product.id, &product.title, &product.coverURL)
+		products = append(products, product)
+	}
+	return products, nil
 }
